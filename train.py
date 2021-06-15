d8390e5ed3e881117676a31258db89b5534644ab
YizheXu <xyabuaa@126.com>
2021-06-15 17:54:06 +0800

    ‘更改训练细节’

--- train.py
+++ train.py
@@ -69,6 +69,7 @@ if __name__ == '__main__':
                 model.save_networks(save_suffix)
 
             iter_data_time = time.time()
+        # 设置最近保存checkpoints
         if epoch % opt.save_epoch_freq == 0:              # cache our model every <save_epoch_freq> epochs
             print('saving the model at the end of epoch %d, iters %d' % (epoch, total_iters))
             model.save_networks('latest')
